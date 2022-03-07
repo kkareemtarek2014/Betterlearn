ed494e4aeae453d45423fabf34451b613f4953f6
kareem <kkareemtarek2@gmail.com>
2022-03-08 00:20:46 +0200

    upload project

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => "https://betterlearnn.herokuapp.com/public/",
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
